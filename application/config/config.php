5918d5d0c2d22bf5c80aaf230879d816d75c8587
rajesh- <r.bhujbal@gmail.com>
2018-07-23 23:40:33 -0700

    Initial Commit

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost/grs';
+$config['base_url'] = 'http://192.168.13.137/grs';
 
 /*
 |--------------------------------------------------------------------------
